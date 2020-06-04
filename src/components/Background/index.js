bfeabff38b42998bd28bd38d082bad6f11716fe2
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-06-04 15:22:03 +0100

    backgroud (wip)

---
+++ src/components/Background/index.js
@@ -0,0 +1,14 @@
+// import { LinearGradient } from 'react-native-linear-gradient';
+import { View } from 'react-native';
+
+import styled from 'styled-components/native';
+
+// export const Background = styled(LinearGradient).attrs({
+//   colors: ['#7159c1', '#ab59c1'],
+// })`
+//   flex: 1;
+// `;
+
+export default styled(View)`
+  flex: 1;
+`;

c1d52f1948a9eaef570a0d4d9155cac2230bf745
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-06-04 12:01:58 +0100

    libs and ios/andooid setup

--- android/app/src/main/java/com/reactnative101/MainActivity.java
+++ android/app/src/main/java/com/reactnative101/MainActivity.java
@@ -1,6 +1,10 @@
 package com.reactnative101;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabldRootView;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "reactnative101";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createReactRootView() {
+        return new RNGestureHandlerEnabldRootView(MainActivity.this);
+      }
+    };
   }
 }
